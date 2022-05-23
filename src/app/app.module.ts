b142d46c3438b26f78b45cd6c18f7710f311c645
Juan Escalada <juanescalada175@gmail.com>
2022-05-23 16:11:30 -0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
+// Defined as a module, with metadata
 @NgModule({
+  // All the components must be imported and added to declarations
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +19,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+// App Modules are a bundle of functionalities for the app
 export class AppModule { }
