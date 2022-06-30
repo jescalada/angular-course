cfa0e09372e70b3949a29b0ed99961c055671922
Juan Escalada <juanescalada175@gmail.com>
2022-06-30 15:54:43 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] // You can define inline styles using styles: ["...", "..."]
 })
 export class AppComponent {
   title = 'my-first-app';
